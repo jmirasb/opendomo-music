7894db7763e8aa3976939e09256e4b2c64650cd8
Javier Miras <jmirasb@gmail.com>
2014-02-01 15:56:42 +0100

    Adding Licence

--- mkpkg.sh
+++ mkpkg.sh
@@ -5,7 +5,7 @@ rm pkg/*.tar.gz 2>/dev/null
 PLUGIN="odmusic"
 DATE=`date +%Y%m%d`
 USR="--owner 1000 --group 1000 --same-permissions"
-EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md'
+EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md --exclude LICENCE'
 
 rm -fr pkg/*.tar.gz
 tar zcf  pkg/$PLUGIN-$DATE.od.noarch.tar.gz usr var  $USR $EXCLUDE
