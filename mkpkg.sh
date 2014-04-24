067249e5ec47d6ff92abb1556630478eb475576d
Javier Miras <jmirasb@gmail.com>
2014-04-24 16:26:30 +0200

    Modify mkpkg

--- mkpkg.sh
+++ mkpkg.sh
@@ -3,9 +3,9 @@
 mkdir -p pkg
 rm pkg/*.tar.gz 2>/dev/null
 PLUGIN="odmusic"
-DATE=`cat var/opendomo/plugins/odmusic.version`
+VERSION=`cat var/opendomo/plugins/odmusic.version`
 USR="--owner 1000 --group 1000 --same-permissions"
 EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md --exclude LICENCE'
 
 rm -fr pkg/*.tar.gz
-tar zcf  pkg/$PLUGIN-$DATE.od.noarch.tar.gz usr var  $USR $EXCLUDE
+tar zcf  pkg/$PLUGIN-$VERSION.tar.gz usr var  $USR $EXCLUDE
