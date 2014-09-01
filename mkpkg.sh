aa98d1365f42fd9d8e1cbc5dc5907e8e0a77522d
Javier Miras Blanco <jmirasb@gmail.com>
2014-09-01 14:44:02 +0200

    Don't use .version file anymore

--- mkpkg.sh
+++ mkpkg.sh
@@ -3,7 +3,7 @@
 mkdir -p pkg
 rm pkg/*.tar.gz 2>/dev/null
 PLUGIN="odmusic"
-VERSION=`cat var/opendomo/plugins/odmusic.version`
+VERSION=`date '+%Y%m%d'`
 USR="--owner 1000 --group 1000 --same-permissions"
 EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md --exclude LICENCE'
 
