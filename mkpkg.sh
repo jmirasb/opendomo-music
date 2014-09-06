334ddaaac029b725129fb6c058c49199f52791ba
Javier Miras Blanco <jmirasb@gmail.com>
2014-09-06 11:18:43 +0200

    Updating mkpkg.sh

--- mkpkg.sh
+++ mkpkg.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-
-mkdir -p pkg
-rm pkg/*.tar.gz 2>/dev/null
-PLUGIN="odmusic"
-VERSION=`date '+%Y%m%d'`
-USR="--owner 1000 --group 1000 --same-permissions"
-EXCLUDE=' --exclude "*~" --exclude .svn --exclude README.md --exclude LICENCE'
-
-rm -fr pkg/*.tar.gz
-tar zcf  pkg/$PLUGIN-$VERSION.tar.gz usr var  $USR $EXCLUDE
+PKGID="odmusic"
+tar cvfz $PKGID-`date '+%Y%m%d'`.tar.gz usr var --owner 1000 --group 1000 --exclude "*~" --exclude .svn --exclude "*.a"
